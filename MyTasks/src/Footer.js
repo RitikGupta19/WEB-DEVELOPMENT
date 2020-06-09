e1295012eb003c3193e3019a5ad0f2bf95bdd5bb
Ritik Gupta <ritikgupta1982@gmail.com>
2020-06-09 16:57:28 +0530

    my tasks app

---
+++ MyTasks/src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+        <div className="footer jumbotron bg-dark text-white">
+        <div className="container">
+          <p className="lead">Task Manger By: <strong>Ritik Gupta </strong></p> 
+          <p className="lead">Made with<strong> React</strong></p>
+        </div>
+        </div>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ MyTasks/src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+
+    return (
+        <div>
+        <div className="jumbotron jumbotron-fluid bg-info text-white">
+        <div className="container">
+          <h1 className="display-4">MY TASKS</h1>
+          <p className="lead">Manage your <strong>Daily Activities</strong>, 
+          become more <strong>Productive</strong> and ahead of others.</p>
+          <p>{window.Date().slice(0,15)}</p>
+        </div>
+        </div>
+        </div>
+    );
+};
+
+export default Header;
