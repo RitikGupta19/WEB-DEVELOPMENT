100c0bab540ec337a20a9d91052392b8a1eb7db3
Ritik Gupta <ritikgupta1982@gmail.com>
2020-10-09 02:02:49 +0530

    some changes

---
+++ DeveloperIn/client/src/components/profiles/ProfileItems.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+const ProfileItems = props => {
+  return (
+    <div>
+      test
+    </div>
+  )
+}
+
+ProfileItems.propTypes = {
+
+}
+
+export default ProfileItems
