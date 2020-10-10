3828a2f28fe85227f5b8ed3507f37fce3ccf9c07
Ritik Gupta <ritikgupta1982@gmail.com>
2020-10-11 01:28:34 +0530

    done some changes

--- DeveloperIn/client/src/components/profile/Profile.js
+++ DeveloperIn/client/src/components/profile/Profile.js
@@ -5,6 +5,8 @@ import { getProfileById } from '../../actions/profile'
 import Spinner from '../layout/Spinner'
 import { Link } from 'react-router-dom';
 import ProfileTop from './ProfileTop'
+import ProfileAbout from './ProfileAbout'
+
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
 
@@ -18,6 +20,7 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
         {auth.isAuthenticated && auth.loading === false && auth.user.user._id === profile.user._id && (<Link to="/edit-profile" className="btn btn-dark">Edit Profile</Link>)}
         <div class="profile-grid my-1">
           <ProfileTop profile={profile} />
+          <ProfileAbout profile={profile} />
         </div>
       </Fragment>)}
     </Fragment>
--- DeveloperIn/client/src/components/profile/ProfileAbout.js
+++ DeveloperIn/client/src/components/profile/ProfileAbout.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
-const ProfileAbout = props => {
+const ProfileAbout = ({ profile: {
+  bio, skills, user: { name }
+} }) => {
   return (
-    <div>
-      About section
+    <div class="profile-about bg-light p-2">
+      {bio && (<Fragment>
+        <h2 class="text-primary">{name}s' Bio</h2>
+        <p>{bio}</p>
+        <div class="line" /></Fragment>)}
+      <h2 class="text-primary">Skill Set</h2>
+      <div class="skills">
+        {skills.map((skill, index) => (
+          <div key={index} className="p-1">
+            <i className="fas fa-check"></i>{skill}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
 
 ProfileAbout.propTypes = {
-
+  profile: PropTypes.object.isRequired,
 }
 
 export default ProfileAbout
