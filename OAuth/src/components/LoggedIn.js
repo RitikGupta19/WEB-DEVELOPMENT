452493fa090639cbb61d071ffe9801c42c86865b
Ritik Gupta <ritikgupta1982@gmail.com>
2020-10-26 23:36:21 +0530

    oauth

--- OAuth/src/components/LoggedIn.js
+++ OAuth/src/components/LoggedIn.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const LoggedIn = () => {
+const LoggedIn = ({ history }) => {
+  const logout = () => {
+    history.replace("/");
+  };
   return (
     <div>
-       You are logged in
+      You are logged in
+      <button className='btn btn-primary' onClick={() => logout()}>
+        Logout
+      </button>
     </div>
   );
-}
+};
 
 export default LoggedIn;
--- OAuth/src/components/NotFound.js
+++ OAuth/src/components/NotFound.js
@@ -1,11 +1,8 @@
-import React from 'react';
+import React from "react";
+import { Redirect } from "react-router-dom";
 
 const NotFound = () => {
-  return (
-    <div>
-       OOOPS -PAGE WAS NOT FOUND!!! 
-    </div>
-  );
-}
+  return <div>OOOPS -PAGE WAS NOT FOUND!!!</div>;
+};
 
 export default NotFound;
